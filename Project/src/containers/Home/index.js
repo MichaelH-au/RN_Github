e5fcce2d32d3e2fe17bc9f080b42f9d3bd49ed5d
Michael <huangjkau@gmail.com>
2019-01-28 23:39:06 +0800

    Bottom bar

--- Project/src/containers/Home/index.js
+++ Project/src/containers/Home/index.js
@@ -1,13 +1,77 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createBottomTabNavigator} from 'react-navigation'
+import {createAppContainer} from 'react-navigation'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import Entypo from 'react-native-vector-icons/Entypo'
+import Popular from '../Propular'
+import Trending  from '../Trending'
+import Favorite from '../Favorite'
+import MyInfo from '../MyInfo'
+
 
 class Index extends Component {
+    _tabNavigator(){
+        return createBottomTabNavigator({
+            PopularPage:{
+                screen:Popular,
+                navigationOptions:{
+                    tabBarIcon:({tintColor, focused}) => (
+                        <MaterialIcons
+                            name={'whatshot'}
+                            size={26}
+                            style={{color:tintColor}}
+                        />
+                    )
+
+
+                }
+            },
+            TrendingPage:{
+                screen:Trending,
+                navigationOptions:{
+                    tabBarIcon:({tintColor, focused}) => (
+                        <Ionicons
+                            name={'md-trending-up'}
+                            size={26}
+                            style={{color:tintColor}}
+                        />
+                    )
+                }
+            },
+            FavoritePage:{
+                screen:Favorite,
+                navigationOptions:{
+                    tabBarIcon:({tintColor, focused}) => (
+                        <MaterialIcons
+                            name={'favorite'}
+                            size={26}
+                            style={{color:tintColor}}
+                        />
+                    )
+                }
+            },
+            MyInfoPage:{
+                screen:MyInfo,
+                navigationOptions:{
+                    tabBarIcon:({tintColor, focused}) => (
+                        <Entypo
+                            name={'user'}
+                            size={26}
+                            style={{color:tintColor}}
+                        />
+                    )
+                }
+            }
+        })
+
+    }
     render() {
+        const Tab = createAppContainer(this._tabNavigator());
         return (
-            <View style={styles.container}>
-                <Text>Home</Text>
-            </View>
-        );
+            <Tab/>
+        )
     }
 }
 
---
+++ Project/src/containers/Trending/index.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class Index extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Trending</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+})
+export default Index;
